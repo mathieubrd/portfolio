187508667c09d9d2655e45caebf3492c952f9a65
Mathieu Brochard <mathieu.brochard@outlook.com>
2024-05-15 10:51:03 +0200

    feat: add aws certifications

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { ExperienceTimeline } from "@/components/ExperienceTimeline"
 import { HeroBanner } from "@/components/HeroBanner"
 import { Me } from "@/components/Me"
 import { Skills } from "@/components/Skills"
+import Image from 'next/image'
+import developerAssociateLogo from "@/public/aws-developer-associate.png"
+import architectAssociateLogo from "@/public/aws-solution-architect-associate.png"
 
 export default function Home() {
   return (
@@ -14,6 +17,16 @@ export default function Home() {
       <h1 className="text-3xl font-bold text-center">My Skills</h1>
 
       <Skills />
+
+      <div className="flex">
+        <div className="relative w-44 h-44">
+          <Image src={developerAssociateLogo} alt="AWS Developer Associate Ceritfication Icon" fill />
+        </div>
+
+        <div className="relative grow w-44 h-44">
+          <Image src={architectAssociateLogo} alt="AWS Solution Architect Associate Ceritfication Icon" fill />
+        </div>
+      </div>
 
       <div className="sm:max-w-lg flex flex-col items-center gap-y-10">
         <h1 className="text-3xl font-bold text-center">
